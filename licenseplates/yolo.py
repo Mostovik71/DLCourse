25777ce77737e8410cf427c1ce03e50c11e8e2e9
Vladislav Mostovik <56130198+Mostovik71@users.noreply.github.com>
2022-10-05 12:01:31 +0300

    Delete yolo.py

--- licenseplates/yolo.py
+++
@@ -1,5 +0,0 @@
-!git clone https://github.com/ultralytics/yolov5  # clone
-%cd yolov5
-%pip install -qr requirements.txt  # install
-!unzip -q /content/drive/MyDrive/PlatesYOLO.zip -d ../data/
-!python /content/yolov5/train.py --img 640 --batch 2 --epochs 20 --data /content/data/data.yaml --weights /content/yolov5/yolov5s.pt --cache
